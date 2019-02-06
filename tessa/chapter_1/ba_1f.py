6a3cfd3aeaf9509b92702752e53dabad75fc2c70
Tessa Pierce <bluegenes@users.noreply.github.com>
2019-02-06 10:34:10 -0800

    Update ba_1f.py

--- tessa/chapter_1/ba_1f.py
+++ tessa/chapter_1/ba_1f.py
@@ -4,27 +4,21 @@ import argparse
 # Find the minimum skew
 
 def find_min_skew(genome):
-    skew = [0]
+    skew_val = 0
     min_skew = 0
     min_skew_pos = []
-    for b in genome:
-       # if G, skew +1 
-        if b.upper() == 'G': 
-            skew_val = skew[-1] + 1
-            skew+=[skew_val]
-       # if C, skew -1, reassess minimum skew 
-        elif b.upper() == 'C': 
-            skew_val = skew[-1] - 1
-            skew+=[skew_val]
+    for i, b in enumerate(genome,1):
+        if b.upper() == 'G':
+            skew_val = skew_val + 1
+        elif b.upper() == 'C':
+            skew_val = skew_val - 1
             if skew_val == min_skew:
-                min_skew_pos+= [len(skew)-1]
+                min_skew_pos.append(i)       
             elif skew_val < min_skew:
                 min_skew = skew_val
-                min_skew_pos = [len(skew)-1]
-        else:
-            # if A or T, add same skew_val to the list of skew_vals
-            skew+=[skew_val]
-    return(set(min_skew_pos))
+                min_skew_pos = [i]
+    return min_skew_pos
+
 
 assert find_min_skew("CCTATCGGTGGATTAGCATGTCCCTGTACGTTTCGCCGCGAACTAGTTCACACGGCTTGATGGCAAATGGTTTTTCCGGCGACCGTAATCGTCCACCGAG") == set([53, 97])
 
